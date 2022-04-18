1a40f63217ba4c38ade0fe1609910e4fe8516292
taky <tkyehab@gmail.com>
2022-04-18 02:06:06 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const expressLayouts = require('express-ejs-layouts')
+
+const indexRouter = require('./routes/index')
+
+app.set('view engine', 'ejs')
+app.set('views',__dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+ 
+
+app.use('/', indexRouter)
+app.listen(process.env.PORT ||port, () => console.log(`Example app listening on port ${port}!`))
